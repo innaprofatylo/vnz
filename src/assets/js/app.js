e6a123b2b7c664933f2869541f0b7fe04c2a86a5
Danil <danil@DanilaPro.local>
2020-02-01 07:53:54 +0200

    fist add

---
+++ src/assets/js/app.js
@@ -0,0 +1,81 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
+// to the top of the file so if we used`import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+// require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+import './lib/foundation-explicit-pieces';
+
+import slick from "slick-carousel";
+
+$(document).foundation();
+
+
+let sliderChoise = $('.ba-slider-choise');
+
+sliderChoise.slick({
+	rows: 2,
+	slidesPerRow: 2,
+	dots: true,
+	dotsClass: 'ba-slick-dots',
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 1080,
+			settings: {
+				rows: 1,
+				slidesPerRow: 2		  
+			}
+		},
+		{
+			breakpoint: 640,
+			settings: {
+			 rows: 1,
+			 slidesPerRow: 1,
+			 dots: false,
+			 arrows: true,
+			 prevArrow: '[data-prev]',
+			 nextArrow: '[data-next]',
+		  }
+		}
+		
+	 ]
+})
+
+let sliderSpec = $('.ba-slider-spec');
+
+sliderSpec.slick({
+	rows: 2,
+	slidesPerRow: 2,
+	dots: true,
+	dotsClass: 'ba-slick-dots',
+	arrows: false,
+	responsive: [
+		{
+			breakpoint: 1080,
+			settings: {
+				rows: 1,
+				slidesPerRow: 2		  
+			}
+		},
+		{
+			breakpoint: 640,
+			settings: {
+			 rows: 1,
+			 slidesPerRow: 1,
+			 dots: false,
+			 arrows: true,
+			 prevArrow: '[data-spec-prev]',
+			 nextArrow: '[data-spec-next]',
+		  }
+		}
+		
+	 ]
+})
